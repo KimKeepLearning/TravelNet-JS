fa1d76a27f03757adbdbd2d08b9389693dd22dc5
Y.Huang <18261195537@163.com>
2020-10-27 21:25:47 +0800

    add recommend

---
+++ src/pages/home/components/recommend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <img class="item-img" :src="item.imgUrl" alt="">
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+                <button class="item-button">查看详情</button>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1608/25/2526fbbad058cd56a3.water.jpg_200x200_00886323.jpg',
+        title: '环岛路帆船出海',
+        desc: '浪漫厦门，浪漫的帆船游行'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1608/25/2526fbbad058cd56a3.water.jpg_200x200_00886323.jpg',
+        title: '环岛路帆船出海',
+        desc: '浪漫厦门，浪漫的帆船游行'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1608/25/2526fbbad058cd56a3.water.jpg_200x200_00886323.jpg',
+        title: '环岛路帆船出海',
+        desc: '浪漫厦门，浪漫的帆船游行'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    display flex
+    height 1.9rem
+    overflow hidden
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        fontsize .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        margin-top .16rem
+        color #fff
+        line-height .44rem
+</style>
---
+++ src/pages/home/components/weekend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <div class="item-img-wrapper">
+                <img class="item-img" :src="item.imgUrl" alt="">
+            </div>
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1511/8f/bf3a40e4afcf29.jpg_r_640x214_90225212.jpg',
+        title: '环岛路帆船出海',
+        desc: '浪漫厦门，浪漫的帆船游行'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1711/15/1cdd5ee885686b.jpg_r_640x214_46768064.jpg',
+        title: '环岛路帆船出海',
+        desc: '浪漫厦门，浪漫的帆船游行'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1509/69/3f535648c26681.jpg_r_640x214_146b5b8e.jpg',
+        title: '环岛路帆船出海',
+        desc: '浪漫厦门，浪漫的帆船游行'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      fontsize .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+    
+</style>
