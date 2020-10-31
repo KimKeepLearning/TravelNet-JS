24577b3e144fb5d26606f694488db9ce50a69fde
Y.Huang <18261195537@163.com>
2020-10-31 18:41:08 +0800

    add touch move alphabet

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list :cities="cities" :hot="hotCities"></city-list>
-        <city-alphabet :cities="cities"></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+        <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
     </div>
 </template>
 
@@ -25,28 +25,28 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
     getCityInfo () {
       axios.get('/api/city.json')
         .then(this.handleGetCityInfoSucc)
-      console.log('getCityInfo', this.cities)
     },
     handleGetCityInfoSucc (res) {
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
         this.cities = data.cities
-        console.log('handleGetCityInfoSucc', this.cities)
-        this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
     this.getCityInfo()
-    console.log('mounted', this.cities)
   }
 }
 </script>
--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -17,7 +17,11 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item, key) of cities" :key="key">
+            <div class="area"
+                 v-for="(item, key) of cities"
+                 :key="key"
+                 :ref="key"
+            >
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
@@ -34,14 +38,22 @@ export default {
   name: 'CityList',
   props: {
     cities: Object,
-    hot: Array
+    hot: Array,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
-    console.log(this.cities['A'])
   },
   updated () {
     this.scroll.refresh()
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 
